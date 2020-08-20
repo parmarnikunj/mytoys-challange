6cd54c8b7827042d8aa1139e2c67caef7123ba30
Parmar, Nikunjkumar <parmar.nikunjkumar@googlemail.com>
2020-08-20 12:29:17 +0200

    initial commit

---
+++ src/main/java/com/mytoys/challange/ProductsChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.mytoys.challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytoys/challange/ProductsChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytoys.challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
